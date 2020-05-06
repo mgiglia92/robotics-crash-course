37528ad78a484c0f68b8844f9d2db78d54d2fafb
mgiglia92 <michael.a.giglia@gmail.com>
2020-05-06 12:56:49 -0400

    GY521 library started

--- Lecture material/arduino_module/arduino_lesson_1/arduino_lesson_1.ino
+++ Lecture material/arduino_module/arduino_lesson_1/arduino_lesson_1.ino
@@ -8,9 +8,14 @@
 #define SERVOPIN 9 //Needs pwm
 #define ACCELPIN 0 //Will be an Analog pin
 #define LEDPIN 3 //Needs pwm
+#define ACCELMAX 10 //Maximum acceleration sensor can output
+#define ACCELMIN -10 //Minimum accel sensor can output
+#define SDA 4 //Data pin for accelerometer
+#define SCL 5 //Clock pin for accelerometer
 
 //Import libraries
 #include <Servo.h>
+#include <GY_521.h>
 //TODO: Add accelorometer library to get data from accelerometer
 
 //Global variables
@@ -19,6 +24,7 @@ int ay; //y-axis
 int az; //z-axis
 Servo servo;
 int brightness; // variable that will control LED brightness
+GY_521 sensor(SDA, SCL); //Accelerometer class
 
 
 void setup() {
@@ -40,4 +46,19 @@ void setup() {
 
 void loop() {
 
+  //Read accelerometer values
+  sensor.update();
+  //TODO: Setup library to read values
+  ax = sensor.get_accel('x');
+  //Convert raw accelerometer values to usable LED brightness values  
+  brightness = raw_accel_to_brightness(ax);
+  analogWrite(LEDPIN, brightness); //NOTE: we use analog write instead of digitalwrite when we want to use PWM on a digital pin.
+}
+
+//Function to convert raw accel values to LED brightness values
+int raw_accel_to_brightness(float raw)
+{
+  //Linearly map the raw values to the brightneess values
+  //NOTE: LED brightness must be a number between 0-255, since PWM is represented with an 8bit number, so 0%-0, 100%-255
+  return map(raw, ACCELMIN, ACCELMAX, 0, 255);
 }
