32b92b0afcbc92f9573322ac3e1b833f1e6b49c5
igvc <igvc@fido.igvc.internal>
2022-06-09 13:15:31 -0400

    Edited bash scripts to allow for installation and activation on both windows and linux machines

--- python_code/activate_venv.sh
+++ python_code/activate_venv.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env sh
 echo "Activating venv"
-cd ..
+
+# TODO: Need to determine how .venv was created, windows directory, python_code/.venv/Scripts/activate
+# For ubuntu directory is, python_code/.venv/bin/activate
 cd $RCC
-source python_code/.venv/Scripts/activate
 cd python_code
+win_dir=".venv/Scripts/"
+lin_dir=".venv/bin/"
+
+if [ -d $win_dir ];
+then
+    echo "Windows" 
+    source .venv/Scripts/activate
+elif [ -d $lin_dir ];
+then 
+    echo "Linux"
+    source .venv/bin/activate
+else 
+    echo "Error, venv activate script not found"
+fi
+
+cd $RCC/python_code
--- python_code/venv_install.sh
+++ python_code/venv_install.sh
@@ -1,11 +1,34 @@
+#TODO: variable shebang???
 #!/usr/bin/env sh
+
+#TODO: Add documentation to let students know to make a system variable RCC with the directory they installed the repository to
+cd $RCC
+cd python_code
 
 echo "Removing .venv"
 rm -r .venv
+
 echo "Making .venv"
-py -m venv .venv
-echo "Activating venv"
-source .venv/Scripts/activate
-where python
+python3 -m venv .venv
+
+# Activate venv to install, depends on windows or ubuntu
+echo "Activating venv"w
+in_dir=".venv/Scripts/"
+lin_dir=".venv/bin/"
+
+if [ -d $win_dir ];
+then
+    echo "Windows" 
+    source .venv/Scripts/activate
+elif [ -d $lin_dir ];
+then 
+    echo "Linux"
+    source .venv/bin/activate
+else 
+    echo "Error, venv activate script not found"
+fi
+
+cd $RCC/python_code
+where python3
 echo "Installing required libraries"
 pip install -r requirements.txt
