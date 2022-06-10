51805ccd827d049f0fbddd11f54d1d5c29d86ba5
Michael Giglia <michael.a.giglia@gmail.com>
2022-06-10 13:36:43 -0400

    holy bash syntax batman

--- python_code/venv_install.sh
+++ python_code/venv_install.sh
@@ -2,20 +2,22 @@
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform 
     echo MACOS
-    machine=MACOS     
+    machine="MACOS"     
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
     echo LINUX
-    machine=LINUX
+    machine="LINUX"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under 32 bits Windows NT platform
     echo WIN32
-    machine=WIN32
+    machine="WIN32"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # Do something under 64 bits Windows NT platform
     echo WIN64
-    machine=WIN64
+    machine="WIN64"
 fi
+
+echo machine_value$machine
 
 cd $RCC
 cd python_code
@@ -25,21 +27,22 @@ rm -r .venv
 
 echo "Making venv"
 
-if machine==WIN64 || machine==WIN32;
-then 
-    python -m venv .venv
-    source .venv/Scripts/activate
-    echo "Windows, Installing required libraries"
-    pip install -r requirements.txt
-elif machine==MACOS || machine==LINUX;
+
+if [ $machine == "MACOS" ] || [ $machine == "LINUX" ];
 then 
     python3 -m venv .venv
     source .venv/bin/activate
     echo "Linux/Mac, Installing required libraries"
     pip install -r requirements.txt
+    echo "Install completed"
+elif [ $machine == "WIN64" ] || [ $machine == "WIN32" ];
+then 
+    python -m venv .venv
+    source .venv/Scripts/activate
+    echo "Windows, Installing required libraries"
+    pip install -r requirements.txt
+    echo "Install completed"
 else 
     echo "Error, machine type not valid"
-    exit
 fi
 
-echo "Install completed"
