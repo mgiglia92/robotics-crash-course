428f16d49eaea56846d2038b7793068055febd00
Michael Giglia <michael.a.giglia@gmail.com>
2022-06-10 13:18:21 -0400

    Fix attempt for venv_install and activate_venv, pushing to test on macos

--- python_code/activate_venv.sh
+++ python_code/activate_venv.sh
@@ -1,25 +1,33 @@
-# TODO: Variable shebang?
-# How for each os?
-#!/usr/bin/env sh
-echo "Activating venv"
-
-# TODO: Need to determine how .venv was created, windows directory, python_code/.venv/Scripts/activate
-# For ubuntu directory is, python_code/.venv/bin/activate
-cd $RCC
-cd python_code
-win_dir=".venv/Scripts/"
-lin_dir=".venv/bin/"
-
-if [ -d $win_dir ];
-then
-    echo "Windows" 
-    source .venv/Scripts/activate
-elif [ -d $lin_dir ];
-then 
-    echo "Linux"
-    source .venv/bin/activate
-else 
-    echo "Error, venv activate script not found"
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform 
+    echo MACOS
+    machine=MACOS     
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    echo LINUX
+    machine=LINUX
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+    echo WIN32
+    machine=WIN32
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+    echo WIN64
+    machine=WIN64
 fi
 
-cd $RCC/python_code
+echo "Activating venv"
+
+cd $RCC
+cd python_code
+
+if machine==WIN64 || machine==WIN32;
+then 
+    source .venv/Scripts/activate
+elif machine==MACOS || machine==LINUX;
+then 
+    source .venv/bin/activate
+else     
+    echo "Error, machine type not valid"
+    exit
+fi
--- python_code/venv_install.sh
+++ python_code/venv_install.sh
@@ -1,39 +1,45 @@
-#TODO: variable shebang???\
-# How for each os?
-#!/usr/bin/env sh
-
 #TODO: Add documentation to let students know to make a system variable RCC with the directory they installed the repository to
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform 
+    echo MACOS
+    machine=MACOS     
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    echo LINUX
+    machine=LINUX
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+    echo WIN32
+    machine=WIN32
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+    echo WIN64
+    machine=WIN64
+fi
+
 cd $RCC
 cd python_code
 
 echo "Removing .venv"
 rm -r .venv
 
+echo "Making venv"
 
-
-# Activate venv to install, depends on windows or ubuntu
-echo "Activating venv"
-in_dir=".venv/Scripts/"
-lin_dir=".venv/bin/"
-
-if [ -d $win_dir ];
-then
-    echo "Windows" 
-    echo "Making .venv"
+if machine==WIN64 || machine==WIN32;
+then 
     python -m venv .venv
     source .venv/Scripts/activate
-elif [ -d $lin_dir ];
+    echo "Installing required libraries"
+    pip install -r requirements.txt
+elif machine==MACOS || machine==LINUX;
 then 
-    echo "Linux"
-    echo "Making .venv"
     python3 -m venv .venv
     source .venv/bin/activate
+    echo "Installing required libraries"
+    pip install -r requirements.txt
 else 
-    echo "Error, venv activate script not found"
+    echo "Error, machine type not valid"
     exit
 fi
 
-cd $RCC/python_code
-echo "Installing required libraries"
-pip install -r requirements.txt
 echo "Install completed"
