8a53b4167842225bbba24dc90703998a3a683fbe
Jacob Koziej <jacobkoziej@gmail.com>
2022-07-07 23:33:10 +0200

    Remove unused functions

--- lib/arduino/rcc/src/actuator/motor.h
+++ lib/arduino/rcc/src/actuator/motor.h
@@ -28,35 +28,5 @@ void motorSetup(void);
  */
 void rawMotorCtrl(int left, int right);
 
-// Conversion utilities
-int convert_pwr_to_pwm(int pwr);
-
-// ------  Motor Control Functions ------
-
-// pwr should always be 0-100
-// commands: 0 - Forward
-//           1 - Backward
-//           2 - Right
-//           3 - Left
-void control(int command, int pwr);
-
-// Cuts power to all motors
-void dwell();
-
-// Moves foward
-void backward(int pwm);
-
-// Moves backward
-void forward(int pwm);
-
-// Rotates left using differential drive style
-void diff_left(int pwm);
-
-// Rotates right using differential drive style
-void diff_right(int pwm);
-
-//control motors directly
-void raw_motor_control(int, int);
-
 
 #endif /* CU_SUMMER_STEM_ROBOTICS_CRASH_COURSE_RCC_ACTUATOR_MOTOR_H */
