f262a2ef064c06b21731ee9fa48fb5a7b0ebac5e
Jacob Koziej <jacobkoziej@gmail.com>
2022-07-09 02:52:21 +0200

    Add `1-variable.ino`

---
+++ lib/arduino/rcc/examples/challenges/00-blink/1-variable/1-variable.ino
@@ -0,0 +1,6 @@
+/*
+ * 1-variable.ino -- variable length blinking
+ *
+ * Write a program that repeatedly blinks the built-in LED for variable
+ * amounts of time ranging from 10ms to 1000ms in increments of 10ms.
+ */
