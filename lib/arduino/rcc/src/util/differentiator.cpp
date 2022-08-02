76f6c0faf903a52d341f81756b4e7ba157cad180
Jacob Koziej <jacobkoziej@gmail.com>
2022-08-02 09:23:49 -0400

    Use tabs over spaces for indentation

--- lib/arduino/rcc/src/util/differentiator.cpp
+++ lib/arduino/rcc/src/util/differentiator.cpp
@@ -11,32 +11,32 @@
 
 Differentiator::Differentiator(float sigma, float ts)
 {
-    this->sigma = sigma;
-    this->ts    = ts;
+	this->sigma = sigma;
+	this->ts    = ts;
 
-    beta = (2.0 * sigma - ts) / (2.0 * sigma + ts);
+	beta = (2.0 * sigma - ts) / (2.0 * sigma + ts);
 }
 
 float Differentiator::differentiate(float y)
 {
-    // calculate derivative
-    y_dot = (beta * y_dot) + (((1 - beta) / ts) * (y - y_d1));
+	// calculate derivative
+	y_dot = (beta * y_dot) + (((1 - beta) / ts) * (y - y_d1));
 
-    y_d1 = y;
+	y_d1 = y;
 
-    return y_dot;
+	return y_dot;
 }
 
 void Differentiator::reset(float degrees)
 {
-    y_dot = 0;
-    y_d1  = degrees;
+	y_dot = 0;
+	y_d1  = degrees;
 }
 
 void Differentiator::setTimeParameters(float ts, float sigma)
 {
-    this->ts    = ts;
-    this->sigma = sigma;
+	this->ts    = ts;
+	this->sigma = sigma;
 
-    beta = (2.0 * sigma - ts) / (2.0 * sigma + ts);
+	beta = (2.0 * sigma - ts) / (2.0 * sigma + ts);
 }
