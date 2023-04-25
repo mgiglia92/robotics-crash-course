26ed00367811214677bf9a850a69ade5e7ad53fe
Michael Giglia <michael.a.giglia@gmail.com>
2023-04-25 17:31:37 -0400

    fixed adc readings

--- dev/pico/tests/src/adc_test.cpp
+++ dev/pico/tests/src/adc_test.cpp
@@ -35,6 +35,20 @@ bool led_state = true;
 //Servo Init
 Servo s1;
 
+/* References for this implementation:
+ * raspberry-pi-pico-c-sdk.pdf, Section '4.1.1. hardware_adc'
+ * pico-examples/adc/adc_console/adc_console.c */
+float convert_temperature(uint16_t raw) {
+    
+    /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
+    const float conversionFactor = 3.3f / (1 << 12);
+
+    float adc = (float)raw * conversionFactor;
+    float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
+
+    return tempC;
+}
+
 
 
 int main() {
@@ -53,17 +67,21 @@ int main() {
 
     //ADC stuff
     adc_init();
-    adc_gpio_init(ADC0);
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+    adc_gpio_init(28);
+    adc_gpio_init(29);
+    adc_set_temp_sensor_enabled(true);
     uint64_t t0, t1, dt;
     while(true)
     {   
         t0 = time_us_64();
 
-        adc_select_input(0);
+        adc_select_input(2);
         auto x = adc_read();
         auto y = gpio_get(22);
-        adc_select_input(4);
-        auto temp = adc_read();
+        adc_select_input(3);
+        auto temp = convert_temperature(adc_read());
         t1 = time_us_64();
         printf("ADC: %i DIGITAL: %i TEMP: %u TIME: %llu\n", x, y, temp,dt);
         dt = t1 - t0;
