83e531e30e7babe0a8c98cc3f258dad7bd7c371c
Michael Giglia <michael.a.giglia@gmail.com>
2023-05-01 00:50:56 -0400

    python test comms

--- dev/pico/python/test_comms.py
+++ dev/pico/python/test_comms.py
@@ -4,7 +4,7 @@ import socket
 import base64
 
 IP_SEND = "192.168.1.37"
-IP_RECV = "192.168.1.35"
+IP_RECV = "192.168.1.38"
 PORT_SEND=9900
 PORT_RECV=9999
 
@@ -15,7 +15,7 @@ try:
     sock.bind((IP_RECV, PORT_RECV))
     print("Try sockopt")
     while True:
-        data, addr = sock.recvfrom(512)
+        data, addr = sock.recvfrom(10)
         print(f"base64decode: {base64.b64decode(data)} | utf8decode: {data.decode('utf-8')}")
 except Exception as e:
     print(e)
