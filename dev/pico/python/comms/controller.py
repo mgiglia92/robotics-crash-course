c3259d9759c3cd40677dde9d82bfe55601ae2af6
Michael Giglia <michael.a.giglia@gmail.com>
2023-05-20 00:12:11 -0400

    Added WirelessController, inherits CommsController

--- dev/pico/python/comms/controller.py
+++ dev/pico/python/comms/controller.py
@@ -7,8 +7,7 @@ from threading import Thread
 import serial
 import time
 import traceback
-
-from comms.packet import *
+import socket
 from comms.messages import *
 
 class CommsController():
@@ -49,7 +48,7 @@ class CommsController():
 		while True:
 			try:
 				if self.ser.inWaiting() > 0:
-					p = Packet.read_from(self.ser)
+					p = packet.Packet.read_from(self.ser)
 					self.inbound.put(p)
 			except:
 				print('exception in inbound loop:')
@@ -63,31 +62,106 @@ class CommsController():
 
 	def get_packet(self):
 		return self.inbound.get()
+	
+class WirelessInterface:
+	picoip = '192.168.1.37'
+	porttopico = 9900
+	computerip = '192.168.1.35'
+	porttocomputer = 9999
+	picohostname = 'PICOHOME'
+	computerhostname = 'localhost'
+
+	def __init__(self, picohostname, picoip, porttopico, computerhostname, computerip, porttocomputer):
+		self.picoip = picoip
+		self.porttopico = porttopico
+		self.computerip = computerip
+		self.porttocomputer = porttocomputer
+		self.picohostname = picohostname
+		self.computerhostname = computerhostname
+
+class WirelessController(CommsController):
+	def __init__(self, interface:WirelessInterface):
+		self.outbound_thread = Thread(
+		target=self.handle_outbound, daemon=True
+		)
+		self.inbound_thread = Thread(
+			target=self.handle_inbound, daemon=True
+		)
+		self.outbound = Queue()
+		self.inbound = Queue()
+
+		# Setup UDP comm interface
+		self.interface = interface
+		self.sockout = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sockin = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		# Check the local ip and pico ip and warn if they are not equivalent
+		try:
+			# computerip = socket.gethostbyname("localhost")
+			# picoip = socket.gethostbyname("picohostname")
+			# assert computerip 	== interface.computerip, 'Computer ip is different'
+			# assert picoip  		== interface.picoip, 'Pico ip is different'
+			self.sockin.bind((self.interface.computerip, self.interface.porttocomputer))
+			self.connected = True
+		except Exception as e:
+			print('exception in setup:')
+			print(traceback.format_exc())
+			self.connected = False
+			print("Socket setup failed")
+		self.outbound_thread.start()
+		self.inbound_thread.start()
+
+	def handle_outbound(self):
+		while True:
+			try:
+				if not self.outbound.empty():
+					p = self.outbound.get()
+					p.wireless_write_to(self)
+			except:
+				print('exception in outbound loop:')
+				print(traceback.format_exc())
+
+	def handle_inbound(self):
+		while True:
+			try:
+				print("Trying to get data from pico")
+				data, addr = self.sockin.recvfrom(512)
+				assert addr[0] == self.interface.picoip, 'Incoming data not from pico'
+				p = Packet.read_from_raw(data)
+				self.inbound.put(p)
+			except:
+				print('exception in inbound loop:')
+				print(traceback.format_exc())
+
+
+def packet_receive(p):
+	if p.id_ == Twist.id():
+		print(f"Received: {Twist(p)}")
+	if p.id == Test_Inbound.id():
+		print(f"Received: {Test_Inbound(p)}")
 
 if __name__ == "__main__":
-	c = CommsController('/dev/ttyACM0')
+	c = WirelessController(WirelessInterface)
 
 	f1 = 0.0
 	f2 = 1.0
 
 	loop_time = 1
 	while True:
-		time.sleep(loop_time/2)
+		time.sleep(loop_time/3)
 
-		tout = Test_Outbound(f1, f2)
+		# tout = Test_Outbound(f1, f2)
+		# pout = tout.pack()
+		# c.send(pout)
+		time.sleep(loop_time/3)
+		tout = Twist((1.5, 0.05))
 		pout = tout.pack()
 		c.send(pout)
 
 		print(f'sending:    {tout}')
 		print(f'packet out: {pout}')
 
-		time.sleep(loop_time/2)
+		time.sleep(loop_time/3)
 		while c.has_packet():
 			pin = c.get_packet()
-			tin = Test_Inbound(pin)
-
-			print(f'packet in: {pin}')
-			print(f'received:  {tin}')
-			f1 = tin.field_1
-			f2 = tin.field_2
-			print()
+			packet_receive(pin)
