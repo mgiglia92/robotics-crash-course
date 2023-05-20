e4897ff81e15050349f5cae6b2affd26427af1ad
Michael Giglia <michael.a.giglia@gmail.com>
2023-05-20 00:13:13 -0400

    dev script to test some stuff

--- dev/pico/python/dev.py
+++ dev/pico/python/dev.py
@@ -1,5 +1,19 @@
 from comms.messages import *
 from comms.serialize import *
 from comms.packet import *
+from comms.controller import *
+import socket
+import time
+# ip_address = socket.gethostbyname("PICOHOME")
+# print(f"pico ip_address: {ip_address}")
+# p = Packet.from_bytes(b'AAAA=92cb9d68f8ec81f3c51db3af82af257d8d99a110')
 
+print("DEBUG")
+w = WirelessController(WirelessInterface)
+# c = CommsController('/dev/ttyACM0')
 
+try:
+    while True:
+        time.sleep(1)
+except Exception as e:
+    print(e)
