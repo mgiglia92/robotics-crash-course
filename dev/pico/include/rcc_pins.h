1c28ebfa167d5dfe1cde944f4143679a38533e76
Michael Giglia <michael.a.giglia@gmail.com>
2023-06-26 00:12:35 -0400

    Added pins designation header file

---
+++ dev/pico/include/rcc_pins.h
@@ -0,0 +1,16 @@
+//Pin Definitions
+//---Motor Controller L298N---//
+#define RCC_ENA 3
+#define RCC_ENB 4
+#define RCC_IN1 5
+#define RCC_IN2 6
+#define RCC_IN3 7
+#define RCC_IN4 8
+//---I2C BUS---//
+#define RCC_I2C_SDA 14
+#define RCC_I2C_SCL 15
+//---SERVO PIN---//
+#define RCC_SERVO 16
+//---PICO ON BOARD INTERFACES---//
+#define RCC_POTENTIOMETER 28
+#define RCC_BUTTON 22
