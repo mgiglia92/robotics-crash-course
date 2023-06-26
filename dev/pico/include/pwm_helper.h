97055eec10ba7bc9a470c5ec1e4788e83755b0a0
Michael Giglia <michael.a.giglia@gmail.com>
2023-06-26 00:12:12 -0400

    Added motor struct and funcs

--- dev/pico/include/pwm_helper.h
+++ dev/pico/include/pwm_helper.h
@@ -1,5 +1,10 @@
 #include "hardware/pwm.h"
 #include "pico/stdlib.h"
+#include <math.h>
+#include <stdlib.h>
+#include "rcc_pins.h"
+
+using namespace std;
 
 typedef struct 
 {
@@ -11,6 +16,16 @@ typedef struct
     bool on;
     bool invert;
 } Servo;
+
+typedef struct
+{
+    Servo left; //ENA pin
+    Servo right; //ENB pin
+    uint in1; //left motor dir pins
+    uint in2;
+    uint in3; //right motor dir pins
+    uint in4;
+} Motor;
 
 uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d)
 {
@@ -45,7 +60,14 @@ void pwm_set_dutyH(uint slice_num, uint chan, int d)
     pwm_set_chan_level(slice_num, chan, pwm_get_wrap(slice_num) * d / 10000);
 }
 
-void ServoInit(Servo *s, uint gpio, bool invert)
+void pwm_set_dutyF(uint slice_num, uint chan, float d)
+{
+    int frac = static_cast<int>(d*10000);
+    frac = min(max(0,frac), 10000);
+    pwm_set_chan_level(slice_num, chan, pwm_get_wrap(slice_num)*frac/10000);
+}
+
+void ServoInit(Servo *s, uint gpio, bool invert, uint freq)
 {
     gpio_init(gpio);
     gpio_set_dir(gpio, true);
@@ -57,8 +79,8 @@ void ServoInit(Servo *s, uint gpio, bool invert)
     pwm_set_enabled(s->slice, false);
     s->on = false;
     s->speed = 0;
-    s->resolution = pwm_set_freq_duty(s->slice, s->chan, 50, 0);
-    pwm_set_dutyH(s->slice, s->chan, 250);
+    s->resolution = pwm_set_freq_duty(s->slice, s->chan, freq, 0);
+    pwm_set_dutyH(s->slice, s->chan, 0);
     if(s->chan)
     {
         pwm_set_output_polarity(s->slice, false, invert);
@@ -86,4 +108,91 @@ void ServoOff(Servo *s)
 void ServoPosition(Servo *s, uint p)
 {
     pwm_set_dutyH(s->slice, s->chan, p*10 + 250);
+}
+
+void MotorsOn(Motor *m)
+{
+    pwm_set_enabled(m->left.slice, true);
+    pwm_set_enabled(m->right.slice, true);
+    m->left.on = true;
+    m->right.on = true;
+}
+
+void MotorsOff(Motor *m)
+{
+
+    pwm_set_enabled(m->left.slice, false);
+    pwm_set_enabled(m->right.slice, false);
+    m->left.on = false;
+    m->right.on = false;
+
+    gpio_put(m->in1, false);
+    gpio_put(m->in2, false);
+    gpio_put(m->in3, false);
+    gpio_put(m->in4, false);
+}
+
+void MotorInitGPIO(uint gpio)
+{
+    gpio_init(gpio);
+    gpio_set_dir(gpio, true);
+}
+
+void MotorInit(Motor *m, uint gpio_l, uint gpio_r, uint freq)
+{
+    // ServoInit(&s->left, RCC_ENA, false, freq);
+    // ServoInit(&s->right, RCC_ENB, false, freq);
+    MotorInitGPIO(gpio_l);
+    MotorInitGPIO(gpio_r);
+    gpio_set_function(gpio_l, GPIO_FUNC_PWM);
+    gpio_set_function(gpio_r, GPIO_FUNC_PWM);
+    m->left.slice = pwm_gpio_to_slice_num(gpio_l);
+    m->right.slice = pwm_gpio_to_slice_num(gpio_r);
+    m->left.chan = pwm_gpio_to_channel(gpio_l);
+    m->right.chan = pwm_gpio_to_channel(gpio_r);
+    m->left.gpio = gpio_l;
+    m->right.gpio = gpio_r;
+    m->left.resolution = pwm_set_freq_duty(m->left.slice, m->left.chan, freq, 0);
+    m->right.resolution = pwm_set_freq_duty(m->right.slice, m->right.chan, freq, 0);
+
+    MotorInitGPIO(RCC_IN1);
+    MotorInitGPIO(RCC_IN2);
+    MotorInitGPIO(RCC_IN3);
+    MotorInitGPIO(RCC_IN4);
+    m->in1 = RCC_IN1;
+    m->in2 = RCC_IN2;
+    m->in3 = RCC_IN3;
+    m->in4 = RCC_IN4;
+}
+
+
+void MotorPower(Motor *m, int lp, int rp)
+{
+    //--Handle Directionality--//
+    if(lp < 0)
+    {
+        gpio_put(m->in1, 0);
+        gpio_put(m->in2, 1);
+    }
+    else 
+    { 
+        gpio_put(m->in1, 1);
+        gpio_put(m->in2, 0);
+    }
+
+    if(rp < 0)
+    {
+        gpio_put(m->in3, 0);
+        gpio_put(m->in4, 1);
+    }
+    else 
+    { 
+        gpio_put(m->in3, 1);
+        gpio_put(m->in4, 0);
+    }
+
+    // pwm_set_dutyF(m->left.slice, m->left.chan, std::abs(lp));
+    // pwm_set_dutyF(m->right.slice, m->right.chan, std::abs(rp));
+    pwm_set_duty(m->left.gpio, abs(lp));
+    pwm_set_duty(m->right.gpio, abs(rp));
 }
