98cc542b9db17d782b711cab4fcf3a627dc208f8
Michael Giglia <michael.a.giglia@gmail.com>
2023-06-29 15:07:33 -0400

    Fixed IRQ bug, missing PICO_MAX_SHARED_IRQ_HANDLERS. Put odom.h back to original state

--- dev/pico/tests/control_test.cpp
+++ dev/pico/tests/control_test.cpp
@@ -16,20 +16,20 @@ int main(void)
     MotorInit(&motors, RCC_ENA, RCC_ENB, 20000);
     MotorsOn(&motors);
 
-    Odom<12> left;
-    Odom<13> right;
+    Left_Odom left;
+    Right_Odom right;
 
-    con.kp = 1;
+    con.kp = 0.1;
     con.ki = 1;
     con.kd = 0.01;
     con.ts = 0.1; // 1ms
-    con.sigma = 0.25;
+    con.sigma = 0.1;
     con.lowerLimit = 0;
     con.upperLimit = 100;
     con.antiWindupEnabled = true;
     
     PID_control controller(con);
-    controller.setDeadbands(0, 30);
+    controller.setDeadbands(0, 60);
     Differentiator diff(con.sigma, con.ts);
 
     adc_select_input(2);
@@ -38,16 +38,16 @@ int main(void)
     while(true)
     {   sleep_ms(con.ts*1000);
         int32_t pot_val = adc_read();
-        float vel_reference = pot_val / 10.0;
+        float vel_reference = (pot_val / 4097.0) * 270;
 
         //Calculate velocity
-        float vel = diff.differentiate(left.getCount());
+        float vel = diff.differentiate(right.getCount());
 
         //Get controller output
         float output = controller.pid(vel_reference, vel);
 
         //Send controller output to motors
-        MotorPower(&motors, output, 0);
+        MotorPower(&motors, 0, output);
         
         cout << "ref: " << vel_reference << " vel: " << vel <<
             " out: " << output << '\n';
